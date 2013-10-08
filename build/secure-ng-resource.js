d821703f2b4b096c8d240340fac998daa8dc3152
David Simon <dsimon@americancouncils.org>
2013-10-08 15:41:06 -0400

    Version 0.3.3

--- build/secure-ng-resource.js
+++ build/secure-ng-resource.js
@@ -2,7 +2,7 @@
 * secure-ng-resource JavaScript Library
 * https://github.com/AmericanCouncils/secure-ng-resource/ 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 10/08/2013 13:19
+* Compiled At: 10/08/2013 15:40
 ***********************************************/
 (function(window) {
 'use strict';

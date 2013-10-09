3a14679ed815cec371db8a34cc8131de1567b075
David Simon <dsimon@americancouncils.org>
2013-10-09 14:47:02 -0400

    Version 0.3.4, renamed component.json to bower.json

--- build/secure-ng-resource.js
+++ build/secure-ng-resource.js
@@ -2,7 +2,7 @@
 * secure-ng-resource JavaScript Library
 * https://github.com/AmericanCouncils/secure-ng-resource/ 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 10/09/2013 14:45
+* Compiled At: 10/09/2013 14:46
 ***********************************************/
 (function(window) {
 'use strict';

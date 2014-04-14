c69714e5c3e9569d6d03e1e5571af6192124ef34
David Simon <david.mike.simon@gmail.com>
2014-04-14 13:19:59 -0400

    Version 0.4.0

--- build/secure-ng-resource.js
+++ build/secure-ng-resource.js
@@ -2,7 +2,7 @@
 * secure-ng-resource JavaScript Library
 * https://github.com/AmericanCouncils/secure-ng-resource/ 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 04/14/2014 13:18
+* Compiled At: 04/14/2014 13:19
 ***********************************************/
 (function(window) {
 'use strict';

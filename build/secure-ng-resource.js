bf2e7f2b28b2c4b0090fe3a995c4bb493482b00c
David Simon <david.mike.simon@gmail.com>
2014-04-14 13:16:17 -0400

    Updated dev libraries

--- build/secure-ng-resource.js
+++ build/secure-ng-resource.js
@@ -2,7 +2,7 @@
 * secure-ng-resource JavaScript Library
 * https://github.com/AmericanCouncils/secure-ng-resource/ 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 03/28/2014 11:34
+* Compiled At: 04/14/2014 13:16
 ***********************************************/
 (function(window) {
 'use strict';
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,30 +1,41 @@
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    '../bower_components/angular/angular.js',
-    '../bower_components/angular-cookies/angular-cookies.js',
-    '../bower_components/angular-resource/angular-resource.js',
-    '../bower_components/angular-mocks/angular-mocks.js',
-    '../build/secure-ng-resource.debug.js',
-    'unit/*Spec.js'
-];
+module.exports = function(config) {
+    config.set({
+        basePath: '..',
 
-// // level of logging
-// // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        frameworks: ['jasmine'],
 
-// // enable / disable colors in the output (reporters and logs)
-colors = true;
+        files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-cookies/angular-cookies.js',
+            'bower_components/angular-resource/angular-resource.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'build/secure-ng-resource.debug.js',
+            'test/unit/*Spec.js'
+        ],
 
-// // Start these browsers, currently available:
-// // - Chrome
-// // - ChromeCanary
-// // - Firefox
-// // - Opera
-// // - Safari
-// // - PhantomJS
-browsers = ['PhantomJS'];
+        // // level of logging
+        // // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-// // Continuous Integration mode
-// // if true, it capture browsers, run tests and exit
-singleRun = true;
+        // // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        // // Start these browsers, currently available:
+        // // - Chrome
+        // // - ChromeCanary
+        // // - Firefox
+        // // - Opera
+        // // - Safari
+        // // - PhantomJS
+        browsers: ['PhantomJS'],
+
+        // // Continuous Integration mode
+        // // if true, it capture browsers, run tests and exit
+        singleRun: true,
+
+        plugins: [
+            'karma-jasmine',
+            'karma-phantomjs-launcher'
+        ]
+    })
+}
